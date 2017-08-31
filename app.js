52600c5eace56155a410d4ecb78f5399b9d71926
Jason L <jluboff@gmail.com>
2017-08-31 16:47:42 -0700

    Moved routes into seperate route file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express'),
       getGames = require('./services/getGames'),
+      routes = require('./routes/routes'),
       session = require('express-session'),
       passport = require('passport'),
       Strategy = require('passport-local').Strategy,
@@ -7,105 +8,55 @@ const express = require('express'),
       flash = require('connect-flash'),
       MongoClient = require('mongodb').MongoClient,
       bcrypt = require('bcryptjs'),
-      //db = require('./models/db'),
       morgan = require('morgan'),
       bodyParser = require('body-parser'),
       port = process.env.PORT || 3000;
 
 MongoClient.connect(`mongodb://localhost:27017/gameTrader`, (err, db) => {
+	passport.use(
+		new Strategy((username, password, cb) => {
+			db.collection('users').findOne({ username }, (err, user) => {
+				if (err) return cb(err);
+				if (!user) return cb(null, false);
+				if (!bcrypt.compareSync(password, user.password))
+					return cb(null, false);
+				return cb(null, user);
+			});
+		})
+	);
 
-passport.use(new Strategy((username, password, cb) => {
-
-  db.collection('users').findOne({username}, (err, user) => {
-    if(err) return cb(err);
-    if(!user) return cb(null, false);
-    if(!bcrypt.compareSync(password, user.password)) return cb(null, false);
-    return cb(null, user);
-  });
-}));
-
-
-passport.serializeUser(function(user, cb) {
-
-	cb(null, user);
-});
-
-passport.deserializeUser(function(username, cb) {
-	db.collection('users').findOne({username}, function(err, user) {
-		if (err) {
-			return cb(err);
-		}
+	passport.serializeUser(function(user, cb) {
 		cb(null, user);
 	});
-});
 
-
-var app = express();
-
-app.set('view engine', 'hbs');
-//app.use(morgan('combined'));
-app.use(flash());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(session({
-  secret: 'potato',
-  resave: true,
-  saveUnitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get('/', (req, res) => {
-	getGames('gears of war', data => {
-		console.log(data);
-		res.send(data);
+	passport.deserializeUser(function(username, cb) {
+		db.collection('users').findOne({ username }, function(err, user) {
+			if (err) {
+				return cb(err);
+			}
+			cb(null, user);
+		});
 	});
-});
 
-app.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), function(req, res) {
-    res.redirect('/');
-  });
-app.get('/login', (req, res) => {
-  res.render('login.hbs');
-})
+	var app = express();
 
-app.get('/login/signup', (req, res) => {
-    let usernameExists = req.flash('exists');
-  res.render('signup.hbs', {usernameExists});
-});
+	app.set('view engine', 'hbs');
+	//app.use(morgan('combined'));
+	app.use(flash());
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(
+		session({
+			secret: 'potato',
+			resave: true,
+			saveUnitialized: true
+		})
+	);
+	app.use(passport.initialize());
+	app.use(passport.session());
 
-app.post('/login/signup', (req, res) => {
-  console.log(req.body);
-  let salt = bcrypt.genSaltSync(10);
-  let hash = bcrypt.hashSync(req.body.password, salt);
+	routes(app, passport, db);
 
-  let user = {
-    username: req.body.username,
-    password: hash,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    city: req.body.city,
-    state: req.body.state
-  };
-  console.log(user);
-  db.collection('users').findOne({username: user.username}, (err, doc) => {
-    if(err) throw err;
-    if(doc){
-      return res.redirect('/usernameExists');
-    } else {
-      db.collection('users').insertOne(user);
-      res.redirect('/login');
-    }
-  })
-
-})
-
-app.get('/usernameExists', (req, res) => {
-  req.flash('exists', 'Username is already taken. Please choose another.');
-  res.redirect('/login/signup');
-})
-
-app.listen(port, () => {
-	console.log(`Listening...`);
-});
-
+	app.listen(port, () => {
+		console.log(`Listening...${port}`);
+	});
 });
---
+++ routes/routes.js
@@ -0,0 +1,69 @@
+const flash = require('connect-flash'),
+      getGames = require('../services/getGames'),
+      bcrypt = require('bcryptjs');
+
+module.exports = (app, passport, db) => {
+	const isLogged = (req, res, next) => {
+		if (req.isAuthenticated()) {
+			return next();
+		}
+		return res.redirect('/login');
+	};
+
+	app.route('/').get((req, res) => {
+		getGames('gears of war', data => {
+			console.log(data);
+			res.send(data);
+		});
+	});
+
+	app
+		.route('/login')
+		.get((req, res) => {
+			res.render('login.hbs');
+		})
+		.post(
+			passport.authenticate('local', { failureRedirect: '/login' }),
+			(req, res) => {
+				res.redirect('/');
+			}
+		);
+
+	app
+		.route('/login/signup')
+		.get((req, res) => {
+			let usernameExists = req.flash('exists');
+			res.render('signup.hbs', { usernameExists });
+		})
+		.post((req, res) => {
+			console.log(req.body);
+			let salt = bcrypt.genSaltSync(10);
+			let hash = bcrypt.hashSync(req.body.password, salt);
+
+			let user = {
+				username: req.body.username,
+				password: hash,
+				firstName: req.body.firstName,
+				lastName: req.body.lastName,
+				city: req.body.city,
+				state: req.body.state
+			};
+			console.log(user);
+			db
+				.collection('users')
+				.findOne({ username: user.username }, (err, doc) => {
+					if (err) throw err;
+					if (doc) {
+						return res.redirect('/usernameExists');
+					} else {
+						db.collection('users').insertOne(user);
+						res.redirect('/login');
+					}
+				});
+		});
+
+	app.route('/usernameExists').get((req, res) => {
+		req.flash('exists', 'Username is already taken. Please choose another.');
+		res.redirect('/login/signup');
+	});
+};
