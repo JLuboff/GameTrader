e2c36bf1237f44d79d8b4a3de9c14b48b3ef7f6f
Jason L <jluboff@gmail.com>
2017-08-31 16:25:15 -0700

    Local authentication is working including hashing the password

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require('express'),
       passport = require('passport'),
       Strategy = require('passport-local').Strategy,
       hbs = require('hbs'),
+      flash = require('connect-flash'),
       MongoClient = require('mongodb').MongoClient,
+      bcrypt = require('bcryptjs'),
       //db = require('./models/db'),
       morgan = require('morgan'),
       bodyParser = require('body-parser'),
@@ -13,33 +15,15 @@ const express = require('express'),
 MongoClient.connect(`mongodb://localhost:27017/gameTrader`, (err, db) => {
 
 passport.use(new Strategy((username, password, cb) => {
-  console.log(username);
+
   db.collection('users').findOne({username}, (err, user) => {
     if(err) return cb(err);
     if(!user) return cb(null, false);
-    if(user.password !== password) return cb(null, false);
+    if(!bcrypt.compareSync(password, user.password)) return cb(null, false);
     return cb(null, user);
   });
 }));
-/*passport.use(
-	new Strategy(function(username, password, cb) {
-		db.users.findByUsername(username, function(err, user) {
-			if (err) {
-				return cb(err);
-			}
 
-			if (!user) {
-				return cb(null, false);
-			}
-
-			if (user.password != password) {
-				return cb(null, false);
-			}
-
-			return cb(null, user);
-		});
-	})
-); */
 
 passport.serializeUser(function(user, cb) {
 
@@ -51,7 +35,6 @@ passport.deserializeUser(function(username, cb) {
 		if (err) {
 			return cb(err);
 		}
-
 		cb(null, user);
 	});
 });
@@ -61,6 +44,7 @@ var app = express();
 
 app.set('view engine', 'hbs');
 //app.use(morgan('combined'));
+app.use(flash());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(session({
   secret: 'potato',
@@ -77,18 +61,47 @@ app.get('/', (req, res) => {
 	});
 });
 
-app.post('/login',
-
-  passport.authenticate('local', { failureRedirect: '/login' }),
-
-  function(req, res) {
-
+app.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), function(req, res) {
     res.redirect('/');
-
   });
 app.get('/login', (req, res) => {
-  console.log(`Loaded.`)
   res.render('login.hbs');
+})
+
+app.get('/login/signup', (req, res) => {
+    let usernameExists = req.flash('exists');
+  res.render('signup.hbs', {usernameExists});
+});
+
+app.post('/login/signup', (req, res) => {
+  console.log(req.body);
+  let salt = bcrypt.genSaltSync(10);
+  let hash = bcrypt.hashSync(req.body.password, salt);
+
+  let user = {
+    username: req.body.username,
+    password: hash,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    city: req.body.city,
+    state: req.body.state
+  };
+  console.log(user);
+  db.collection('users').findOne({username: user.username}, (err, doc) => {
+    if(err) throw err;
+    if(doc){
+      return res.redirect('/usernameExists');
+    } else {
+      db.collection('users').insertOne(user);
+      res.redirect('/login');
+    }
+  })
+
+})
+
+app.get('/usernameExists', (req, res) => {
+  req.flash('exists', 'Username is already taken. Please choose another.');
+  res.redirect('/login/signup');
 })
 
 app.listen(port, () => {
