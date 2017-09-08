7e629c89ef0869f02cac8c971f193ce780f1599d
Jason L <jluboff@gmail.com>
2017-09-08 16:33:11 -0700

    Few additional changes

--- services/getGames.js
+++ services/getGames.js
@@ -1,36 +1,34 @@
 const querystring = require('querystring'),
-      https = require('https');
+	    https = require('https');
 
 const getGames = function(game, callback) {
-  /*const apiQuery = querystring.stringify({
-      'fields':
-  }); */
+	const options = {
+		hostname: 'api-2445582011268.apicast.io',
+		headers: {
+			'user-key': 'f07eb68ccc69683f2d96ea50b9820ddd',
+			Accept: 'application/json'
+		},
+		port: 443,
+		path: `/games/?search=${game.replace(
+			/\s/g,
+			'%20'
+		)}&fields=id,name,summary,total_rating,developers.name,publishers.name,game_modes.name,cover,release_dates.platform,release_dates.human&expand=developers,publishers,game_modes&filter[release_dates.platform][eq]=48&filter[release_dates.platform][eq]=49&limit=5`,
+		method: 'GET'
+	};
 
-  const options = {
-    hostname: 'api-2445582011268.apicast.io',
-    headers: {
-      'user-key': 'f07eb68ccc69683f2d96ea50b9820ddd',
-      'Accept' : 'application/json'
-    },
-    port: 443,
-    path: `/games/?search=${game.replace(/\s/g, '%20')}&fields=id,name,summary,total_rating,developers.name,publishers.name,game_modes.name,cover,release_dates.platform,release_dates.human&expand=developers,publishers,game_modes&filter[release_dates.platform][eq]=48&filter[release_dates.platform][eq]=49&limit=5`,
-    method: 'GET'
-  };
+	console.log(options);
+	https.get(options, res => {
+		let data = '';
+		res.setEncoding('utf8');
 
-  console.log(options);
-  https.get(options, res => {
-    let data = '';
-    res.setEncoding('utf8');
+		res.on('data', chunk => {
+			data += chunk;
+		});
 
-    res.on('data', chunk => {
-    //  console.log(chunk);
-      data += chunk;
-    });
-
-    res.on('end', () => {
-      return callback(JSON.parse(data));
-    });
-  });
+		res.on('end', () => {
+			return callback(JSON.parse(data));
+		});
+	});
 };
 
 module.exports = getGames;
